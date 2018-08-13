9db91f1127e62afe8419c4e397e2ecccdc6e6b93
valerybriz <valerybriz@gmail.com>
2018-08-13 16:49:16 -0500

    adding try catch for app initialization

--- app.py
+++ app.py
@@ -8,20 +8,23 @@ cwd = os.getcwd() # used by static file server
 # execute asynchronously action
 # print('response', WSHandler.get_repo_pages('Prescrypto/cryptosign_whitepaper/', 'README.md'))
 
-'''Initializing the application with routes'''
-web_app = Application([
-    (r"/api/v1/helloworld", routes.HelloWorld),
-    (r"/api/v1/renderrepohash", routes.PostRepoHash),
-    (r"/api/v1/renderurl", routes.RenderUrl),
-    (r"/api/v1/auth/login", routes.AuthLoginHandler),
-    (r"/api/v1/auth/signin", routes.RegisterUser),
-    (r"/api/v1/git/(.*)", FallbackHandler, dict(fallback=oauth_app)),
-    (r"/(.*\.css)", StaticFileHandler, {"path": cwd}),
-    (r'.*', routes.APINotFoundHandler)],
-    debug=True)
+try:
+    '''Initializing the application with routes'''
+    web_app = Application([
+        (r"/api/v1/helloworld", routes.HelloWorld),
+        (r"/api/v1/renderrepohash", routes.PostRepoHash),
+        (r"/api/v1/renderurl", routes.RenderUrl),
+        (r"/api/v1/auth/login", routes.AuthLoginHandler),
+        (r"/api/v1/auth/signin", routes.RegisterUser),
+        (r"/api/v1/git/(.*)", FallbackHandler, dict(fallback=oauth_app)),
+        (r"/(.*\.css)", StaticFileHandler, {"path": cwd}),
+        (r'.*', routes.APINotFoundHandler)],
+        debug=True)
 
 
-application = WSGIAdapter(web_app)
+    application = WSGIAdapter(web_app)
+except Exception as e:
+    print(e)
 
 
 
