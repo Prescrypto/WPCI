65d4cad0a4e4591e140f355d13f66a6b12172895
valerybriz <valerybriz@gmail.com>
2018-08-27 16:18:36 -0500

    added comments

--- handlers/WSHandler.py
+++ handlers/WSHandler.py
@@ -13,6 +13,7 @@ headers = conf.headers
 GIT_BASE_URI = conf.GITHUB_API_URL
 
 def get_nda(payload):
+    '''this function creates a new page dynamically by storing the payload posted into the data base'''
     URL= conf.CRYPTO_SIGN_URL
     SIGN_URL = 'api/v1/sign/'
     TOKEN_URL = 'oauth/token/'
