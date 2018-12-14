37c34e4fb95c298f5745bb26ef4085a40a0422a2
valerybriz <valerybriz@gmail.com>
2018-12-14 14:29:28 -0600

    added script to update the password

--- reset_password.py
+++ reset_password.py
@@ -1,13 +1,27 @@
 import logging
-#mongodb
-from pymongo import MongoClient
-from pymongo.collation import Collation
-from bson.objectid import ObjectId
+
 #internal
 import config as conf
+from models import User
 
 
 # Load Logging definition
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('tornado-info')
 
+
+
+
+
+if __name__ == "__main__":
+    print("Replace the password of an User:")
+    print("please enter the username followed by the new password no spaces as following:")
+    admininput =input("myuser@organization,MyNewPassword")
+    try:
+        inputarray = admininput.split(",")
+        user = User.User(inputarray[0])
+        if user.find() is False:
+            print("there is no such a user")
+        user.validate_email(inputarray[1])
+    except Exception as e:
+        print(e)
