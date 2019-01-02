f9a63fbabc6ab27e0a65df612dd02c72e0f1e4ba
valery briz <valerybriz@gmail.com>
2019-01-02 17:36:52 -0600

    changed json header on post

--- handlers/WSHandler.py
+++ handlers/WSHandler.py
@@ -45,6 +45,7 @@ def get_nda(payload):
             #if there is a token in the payload then request the pdf
             headers["Authorization"] = "Bearer " + token_json_result.get("access_token")
             sign_result = requests.post(url=URL + SIGN_URL, data=payload, headers=headers)
+            print(sign_result.content)
 
             return sign_result.content
     except Exception as e:
