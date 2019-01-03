790540348cf86c2852f5ca1d0ba3d986692458ac
valery briz <valerybriz@gmail.com>
2019-01-02 18:01:12 -0600

    changed to json dumps on post

--- handlers/WSHandler.py
+++ handlers/WSHandler.py
@@ -43,8 +43,9 @@ def get_nda(payload):
 
         if token_json_result.get("access_token"):
             #if there is a token in the payload then request the pdf
-            headers['Authorization'] = 'Bearer ' + token_json_result.get("access_token")
-            sign_result = requests.post(url=URL + SIGN_URL, json=payload, headers=headers)
+            headers["Accept"] = "text/plain"
+            headers['Authorization'] = "Bearer " + token_json_result.get("access_token")
+            sign_result = requests.post(url=URL + SIGN_URL, data=json.dumps(payload), headers=headers)
             print(sign_result.content)
 
             return sign_result.content
