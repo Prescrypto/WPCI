393f08b3913287a24719ff5f7b2e064ec8aab745
valery briz <valerybriz@gmail.com>
2019-01-10 17:45:41 -0600

    added new models for rexchain use

---
+++ models/signerUser.py
@@ -0,0 +1,144 @@
+#python
+import time
+import logging
+
+#mongodb
+import pymongo
+from pymongo import MongoClient
+from passlib.context import CryptContext
+
+#internal
+from models.mongoManager import ManageDB
+import config as conf
+from utils import get_hash
+
+# Load Logging definition
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('tornado-info')
+
+
+class SignerUser(object):
+
+    def __init__(self, email=None, name=None):
+        self.email = email
+        self.name = name
+        self.pub_key = None
+        self.priv_key = None
+
+    def __str__(self):
+        return "SignerUser(email='%s')" % self.email
+
+    def set_attributes(self, dictattr ):
+        self.__dict__.update(dictattr)
+
+    def __setitem__(self, name, value):
+        self.__dict__[name] = value
+
+
+    def find(self):
+        '''finds a signer user by the email'''
+        result = False
+        mydb = None
+        try:
+            collection = "SignerUser"
+            mydb = ManageDB(collection)
+            docs = mydb.select("email", self.email)
+            if len(docs) > 0:
+                result = True
+
+        except Exception as e:
+            logger.info("finding signer user"+ str(e))
+
+        finally:
+            if mydb is not None:
+                mydb.close()
+
+        return result
+
+    def find_by_attr(self, key, value):
+        '''finds a user object by the attribute id'''
+        result = False
+        mydb = None
+        try:
+            collection = "SignerUser"
+            mydb = ManageDB(collection)
+            docs = mydb.select(key, value)
+            if len(docs) > 0:
+                self.__dict__ = docs[0]
+                return self
+            else:
+                logger.info("signer user not found")
+
+        except Exception as e:
+            logger.info("finding signer user" + str(e))
+
+        finally:
+            if mydb is not None:
+                mydb.close()
+
+        return result
+
+    def create(self):
+        '''creates a new user on the bd'''
+        result = False
+        mydb = None
+        try:
+            collection = "SignerUser"
+            mydb = ManageDB(collection)
+
+            if not self.pub_key or not self.priv_key:
+                # call generate public and private key methods
+                print("generate a new keys")
+            else:
+                logger.info("keys already created")
+
+            result = mydb.insert_json(self.__dict__)
+
+        except Exception as error:
+            logger.info("creating user", error)
+
+        finally:
+            if mydb is not None:
+                mydb.close()
+
+        return result
+
+    def update(self):
+        '''updates a email on the bd'''
+        result = None
+        mydb = None
+        try:
+            collection = "SignRecord"
+            mydb = ManageDB(collection)
+            temp_user = self.__dict__
+
+            result = mydb.update({"email": self.email}, temp_user)
+
+        except Exception as error:
+            logger.info("updating email "+ str(error))
+            result = None
+
+        finally:
+            if mydb is not None:
+                mydb.close()
+
+        return result
+
+    def get_attribute(self, attribute_name):
+        '''gets the attribute from the bd'''
+        result = False
+        mydb = None
+        try:
+            collection = "SignRecord"
+            mydb = ManageDB(collection)
+            docs = mydb.select("email", self.email)
+            if len(docs) > 0:
+                result = docs[0].get(attribute_name)
+        except:
+            result = None
+
+        finally:
+            if mydb is not None:
+                mydb.close()
+
+        return result
