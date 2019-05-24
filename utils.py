197a12279c74a477a2583b002414863cddf1956a
Valery Briz <valerybriz@gmail.com>
2019-05-24 15:49:40 -0500

    update the email validation filter

--- utils.py
+++ utils.py
@@ -57,7 +57,7 @@ def get_hash(strings_list, hashes_list=[]):
 
 
 def is_valid_email(email):
-    return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+    return bool(re.search(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email))
 
 
 def allowed_file(filename):
